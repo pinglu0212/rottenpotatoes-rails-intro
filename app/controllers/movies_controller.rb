fa610cdd040870ccdb390f9c80e8f50f902fe812
EC2 Default User <ec2-user@ip-172-31-44-230.us-east-2.compute.internal>
2020-02-16 06:06:31 +0000

    part 3 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -12,17 +12,6 @@ class MoviesController < ApplicationController
 
 
   def index
-      @all_ratings = Movie.all_ratings
-      if params[:ratings]
-          session[:ratings] = params[:ratings]
-          @selected_ratings = session[:ratings].keys
-      elsif session[:ratings]
-          @selected_ratings = session[:ratings].keys
-      else
-          @selected_ratings = @all_ratings
-      end
-      
-      @movies = @movies.where(:rating => @selected_ratings)
       if params[:sort]
           session[:sort] = params[:sort]
           @movies = Movie.all.order(params[:sort])
@@ -31,8 +20,19 @@ class MoviesController < ApplicationController
       else
           @movies = Movie.all
       end
-
+      @all_ratings = Movie.all_ratings
+      if params[:ratings]
+          @selected_ratings = params[:ratings].keys
+          session[:ratings] = params[:ratings]
+          @selected_ratings = session[:ratings].keys
+      elsif session[:ratings]
+          @selected_ratings = session[:ratings].keys
+      else
+          @selected_ratings = @all_ratings
+      end
+      @movies = @movies.where(:rating => @selected_ratings)
   end
+
 
 
 
